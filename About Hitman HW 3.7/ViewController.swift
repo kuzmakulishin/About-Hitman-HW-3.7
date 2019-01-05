4e98c3a01de7aafde86ef0d73838297526fdc9fe
Kuzma <kuzmakulisin@macbook-pro-kuzma.local>
2019-01-05 13:00:06 +0400

    Initial Commit

---
+++ About Hitman HW 3.7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About Hitman HW 3.7
+//
+//  Created by Kuzma on 05/01/2019.
+//  Copyright © 2019 Kuzma Kulishin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
